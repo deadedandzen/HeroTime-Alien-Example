d94d77ca1573bea64a5d45135e8c738ffcd65ed0
deaded <104146022+deadedandzen@users.noreply.github.com>
2024-10-21 22:47:37 +0100

    new alien database

--- addon/grave.ht/kubejs_scripts/alien_database.js
+++ addon/grave.ht/kubejs_scripts/alien_database.js
@@ -1,2 +1,17 @@
-global.grave_alien_101 = ['test_alien']
-//
+function Alien(name, can_ult, can_kix, unlockable, randomizer) {
+    this.name = name;
+    //tells you the aliens name
+    this.can_ult = can_ult;
+    //tells you if the alien can go ultimate
+    this.can_kix = can_kix;
+    //tells you if the alien can go omnikix
+    this.unlockable = unlockable;
+    //tells you if the alien is unlockable
+    this.randomizer = randomizer;
+    //tells you if the alien is on the randomizer
+};
+
+
+
+global.grave_alien_101 = new Alien("test_alien", false, false, false, false);
+//global.grave_alien_101 = ['test_alien'] <- the old version
